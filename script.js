c8f6241c3bd63fa84a7d8fd2a572da842ac452f2
Unknown User <adityasinghchandel961@gmail.com>
2026-07-13 10:49:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -304,3 +304,21 @@ function closeSuccessModal() {
     ).style.display = "none";
 }
 
+// FAQ Toggle
+
+const faqItems =
+document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+    const question =
+    item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+
+        item.classList.toggle("active");
+
+    });
+
+});
+
